adf0f296b4797ca2b1f0ba8a6d7ee79228b8cdd6
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-01 17:36:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -126,7 +126,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            cd bums && node bot
+            cd bums && node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
